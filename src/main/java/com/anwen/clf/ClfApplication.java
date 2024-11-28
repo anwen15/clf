72d340eb7d986eb94244078c46d03b2c43845d6d
anwen15 <2876709381@qq.com>
2024-11-28 20:53:42 +0800

    项目创建

---
+++ src/main/java/com/anwen/clf/ClfApplication.java
@@ -0,0 +1,13 @@
+package com.anwen.clf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anwen/clf/ClfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anwen.clf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
